6cbc7639d631c835aa27b4e45ad130d7023b0dea
ThallesDaniel <th4llesdaniel@gmail.com>
2025-08-19 13:55:58 -0300

    Initial project structure setup

---
+++ src/main/java/com/ThallesDaniel/criptopay/CriptopayApplication.java
@@ -0,0 +1,18 @@
+package com.ThallesDaniel.criptopay;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class CriptopayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(CriptopayApplication.class);
+        app.setBannerMode(Banner.Mode.CONSOLE);
+        app.run(args);
+
+    }
+}
---
+++ src/test/java/com/ThallesDaniel/criptopay/CriptopayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ThallesDaniel.criptopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptopayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
